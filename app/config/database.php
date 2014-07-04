5645c356cb90bb85983cdff7546bf4f8fb808e32
kiritvijay <kiritvijay@langoor.com>
2014-07-04 21:03:31 +0530

    ad

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'vcv',
 			'username'  => 'root',
-			'password'  => 'zovi',
+			'password'  => 'vijaybhava',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
